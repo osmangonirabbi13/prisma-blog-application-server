65ffc421c7fec9f0a8502e7a5c8cad68f2c465fe
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-12-31 12:26:09 +0600

    email verification setup

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,13 @@ import express from "express";
 import { postRouter } from "./modules/post/post.routes";
 import { auth } from "./lib/auth";
 import { toNodeHandler } from "better-auth/node";
-
+import cors from 'cors'
 const app = express();
+
+app.use(cors({
+  origin: process.env.APP_URL,
+  credentials : true
+}))
 
 app.all("/api/auth/*splat", toNodeHandler(auth));
 
